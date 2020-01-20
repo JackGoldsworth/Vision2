33e8aba0a56457a480d882a689f8cf92154db5b4
Jack Goldsworth <JackGoldsworth@gmail.com>
2020-01-19 23:32:22 -0600

    Planning on overhauling the entire project.

--- src/main/kotlin/me/jackgoldsworth/vision/Vision.kt
+++ src/main/kotlin/me/jackgoldsworth/vision/Vision.kt
@@ -1,21 +1,9 @@
 package me.jackgoldsworth.vision
 
-import me.jackgoldsworth.vision.commands.CommandExecutor
-import me.jackgoldsworth.vision.commands.CommandHandler
-import me.jackgoldsworth.vision.handlers.SpotifyHandler
-
 object Vision {
-
-    private val spotify = SpotifyHandler()
-    private lateinit var commandHandler: CommandHandler
-    private lateinit var commandExecutor: CommandExecutor
 
     @JvmStatic
     fun main(args: Array<String>) {
-        spotify.connect()
-        commandHandler = CommandHandler(spotify.spotifyApi)
-        commandHandler.registerCommands()
-        commandExecutor = CommandExecutor(commandHandler)
-        commandExecutor.listen()
+
     }
 }
