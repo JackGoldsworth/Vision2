ea1139208c7da2a5da0add5ff01b7a676a9612ab
JackGoldsworth <jackgoldsworth@gmail.com>
2020-01-28 15:50:52 -0600

    tried to make things a bit less repetitive

--- core/src/main/kotlin/me/jackgoldsworth/webapp/command/CommandParser.kt
+++ core/src/main/kotlin/me/jackgoldsworth/webapp/command/CommandParser.kt
@@ -2,16 +2,17 @@ package me.jackgoldsworth.webapp.command
 
 import me.jackgoldsworth.webapp.command.impl.PlaySongCommand
 import me.jackgoldsworth.webapp.command.impl.VolumeCommand
+import me.jackgoldsworth.webapp.utils.StringUtils
 
 class CommandParser(private val command: String, private val auth: String?) {
 
     private val splitCommand = command.split(" ")
 
     fun parse(): Command? {
-        if (splitCommand[0].equals("spotify", ignoreCase = true)) {
-            if (splitCommand[1].equals("play", ignoreCase = true)) {
+        if (StringUtils.equals(splitCommand[0], "spotify")) {
+            if (StringUtils.equals(splitCommand[1], "play")) {
                 return PlaySongCommand(splitCommand, auth ?: throw IllegalStateException("Auth token was null"))
-            } else if (splitCommand[1].equals("volume", ignoreCase = true)) {
+            } else if (StringUtils.equals(splitCommand[1], "volume")) {
                 return VolumeCommand(splitCommand, auth ?: throw IllegalStateException("Auth token was null"))
             }
         }
---
+++ core/src/main/kotlin/me/jackgoldsworth/webapp/utils/StringUtils.kt
@@ -0,0 +1,17 @@
+package me.jackgoldsworth.webapp.utils
+
+/**
+ * @author Jack Goldsworth
+ * @since v0.1
+ */
+object StringUtils {
+
+    /**
+     * Checks if two strings are equals with using ignore
+     * case always true.
+     * @return equals with ignore case.
+     */
+    fun equals(str1: String, str2: String): Boolean {
+        return str1.equals(str2, ignoreCase = true)
+    }
+}
